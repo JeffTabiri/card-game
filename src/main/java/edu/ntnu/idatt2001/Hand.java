0c1248a09e5189be19d3094a29cadf645567aac2
tabir <tabirijeffrey@gmail.com>
2023-03-04 13:46:16 +0100

    Hand class created

---
+++ src/main/java/edu/ntnu/idatt2001/Hand.java
@@ -0,0 +1,26 @@
+package edu.ntnu.idatt2001;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+  private List<Card> cards;
+
+  public Hand() {
+    cards = new ArrayList<>();
+  }
+
+  public void addCard(Card card) {
+    cards.add(card);
+  }
+
+  public void removeCard(Card card) {
+    cards.remove(card);
+  }
+
+  public List<Card> getCards() {
+    return cards;
+  }
+
+
+}
